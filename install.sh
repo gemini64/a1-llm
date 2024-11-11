52f41a73b3d9a85942302f8f19ba6da28fcece93
¨Eugenio <eugenio.marzona@gmail.com>
2024-11-11 17:05:05 +0100

    added install script

---
+++ install.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+PY_VENV=".venv";
+ENV_TEMPLATE="./.env_sample";
+ENV_TARGET="./.env";
+GEN_SCRIPT="./gen.py"
+
+# colors for readability
+GREEN='\033[0;32m';
+LIGHT_BLUE='\033[1;34m';
+YELLOW='\033[0;33m';
+RED='\033[0;31m';
+NC='\033[0m'; # No Color
+
+# local settings
+echo -e "${LIGHT_BLUE}[1/4]${NC} - ${GREEN}Setting up .env local settings${NC}";
+if [ ! -f "$ENV_TARGET" ]; then
+    cp "$ENV_TEMPLATE" "$ENV_TARGET";
+fi
+
+# venv
+echo -e "${LIGHT_BLUE}[2/4]${NC} - ${GREEN}Setting up python venv${NC}";
+python3 -m venv "$PY_VENV";
+source "${PY_VENV}"/bin/activate;
+
+# other requirements
+echo -e "${LIGHT_BLUE}[3/4]${NC} - ${GREEN}Installing requirements${NC}";
+pip install -r requirements.txt;
+
+# generate prompts
+echo -e "${LIGHT_BLUE}[4/4]${NC} - ${GREEN}Generating user prompts${NC}";
+python "${GEN_SCRIPT}";
+
+# exit
+deactivate;
+echo -e "${LIGHT_BLUE}[DONE]${NC} - ${GREEN}Remember to activate the venv!${NC}"
