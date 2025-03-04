84234264da6edd4d8fedb452f923cf3a565640f9
¨Eugenio <eugenio.marzona@gmail.com>
2025-03-04 14:42:07 +0100

    Added metadata extraction script also on tatoeba

--- datasets/cefr_leveled_en/metadata.py
+++ datasets/cefr_leveled_en/metadata.py
@@ -1,8 +1,14 @@
 import pandas as pd
 
+###
+# Data is hosted on Kaggle:
+#     https://www.kaggle.com/datasets/amontgomerie/cefr-levelled-english-texts
+#
+# Used export is:
+#     - raw dataset ('cefr_leveled_texts.csv')
+###
 
-
-# --- ifles
+# --- files
 output_file = "./metadata.tsv"
 input_file = "./cefr_leveled_texts.csv"
 
@@ -22,20 +28,20 @@ for label in labels:
     subset = df[df["label"] == label]
 
     words_meta[label] = {
-        "words_min": int(subset["words"].min()),
-        "words_max": int(subset["words"].max()),
-        "words_mean": int(subset["words"].mean()),
-        "words_median": int(subset["words"].median()),
-        "words_mode": int(subset["words"].mode()[0])
+        "words_min": round(subset["words"].min()),
+        "words_max": round(subset["words"].max()),
+        "words_mean": round(subset["words"].mean()),
+        "words_median": round(subset["words"].median()),
+        "words_mode": round(subset["words"].mode()[0])
     }
 
 # --- also add data for all
 words_meta["ALL"] = {
-    "words_min": int(df["words"].min()),
-    "words_max": int(df["words"].max()),
-    "words_mean": int(df["words"].mean()),
-    "words_median": int(df["words"].median()),
-    "words_mode": int(df["words"].mode()[0])
+    "words_min": round(df["words"].min()),
+    "words_max": round(df["words"].max()),
+    "words_mean": round(df["words"].mean()),
+    "words_median": round(df["words"].median()),
+    "words_mode": round(df["words"].mode()[0])
 }
 
 # --- output data
